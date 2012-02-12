3020eba45b2b9dbcb9823704d8b9b54f6cfd0825
Omer Cansizoglu <ocansizoglu@totalccs.com>
2012-02-11 22:36:09 -0500

    Initial Commit

---
+++ RemindIt/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  RemindIt
+//
+//  Created by Omer Cansizoglu on 2/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ RemindIt/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  RemindIt
+//
+//  Created by Omer Cansizoglu on 2/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
