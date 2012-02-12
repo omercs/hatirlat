fe7b07bf91c956aa992cf06a7b368c814bd841a3
Omer Cansizoglu <ocansizoglu@totalccs.com>
2012-02-11 23:09:05 -0500

    add other controllers

---
+++ RemindIt/AccountController.h
@@ -0,0 +1,13 @@
+//
+//  AccountController.h
+//  RemindIt
+//
+//  Created by Omer Cansizoglu on 2/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountController : UIViewController
+
+@end
---
+++ RemindIt/ContactsController.h
@@ -0,0 +1,13 @@
+//
+//  Contacts.h
+//  RemindIt
+//
+//  Created by Omer Cansizoglu on 2/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsController : UIViewController
+
+@end
