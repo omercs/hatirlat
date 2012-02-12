49f9f4c8641d0ad7335537872f90c6cd0a902e46
Omer Cansizoglu <ocansizoglu@totalccs.com>
2012-02-11 22:52:19 -0500

    three20,restkit others

--- RemindIt/AppDelegate.h
+++ RemindIt/AppDelegate.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-
+#import "Three20/Three20.h"
 @interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
